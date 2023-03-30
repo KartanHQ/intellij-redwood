155ad61b7706b03d560c1a454bee981e4869044c
Milad Nekofar <milad@nekofar.com>
2023-03-30 22:11:05 +0330

    refactor: update plugin icon and add extra icons

---
+++ src/main/kotlin/com/nekofar/milad/intellij/redwood/RedwoodIcons.kt
@@ -0,0 +1,11 @@
+package com.nekofar.milad.intellij.redwood
+
+import com.intellij.openapi.util.IconLoader
+
+object RedwoodIcons {
+    @JvmField
+    val FileType = IconLoader.getIcon("/icons/type.svg", javaClass)
+
+    @JvmField
+    val ProjectGenerator = IconLoader.getIcon("/icons/redwood.svg", javaClass)
+}
